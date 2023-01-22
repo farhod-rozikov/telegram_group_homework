4965a0e0d9ab1d7004d8a338f43c99ded5f18bef
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-22 16:42:29 +0500

    find users name

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -1,3 +1,5 @@
+from timeit import timeit
+
 from read_data import read_data
 
 def find_all_users_name(data: dict)->list:
@@ -9,4 +11,8 @@ def find_all_users_name(data: dict)->list:
     Returns:
         list: List containing all the users name.
     """
-    return
+    return list(set([_uid['from'] for _uid in data['messages'] if _uid['type'] == "message"]))
+
+data = read_data('data/result.json')
+if __name__ == '__main__':
+    print(find_all_users_name(data))
